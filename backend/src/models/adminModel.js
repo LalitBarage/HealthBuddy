bc539d4c4b8a29a03d01ebf16808a63a7f30ef6c
LalitBarage <lalitbarage18092004@gmail.com>
2025-04-17 04:17:54 +0530

    fix: correct SQL query to use sub_Dist instead of subDist in findSchemeBySubDist function

--- backend/src/models/adminModel.js
+++ backend/src/models/adminModel.js
@@ -2,9 +2,10 @@ const db = require("../config/db");
 
 const findSchemeBySubDist = async (subDist) => {
   try {
-    const schemes = await db.query(`SELECT * FROM schemes WHERE subDist = $1`, [
-      subDist,
-    ]);
+    const schemes = await db.query(
+      `SELECT * FROM schemes WHERE sub_Dist = $1`,
+      [subDist]
+    );
     return schemes.rows;
   } catch (error) {
     console.error("Error in findSchemeBySubDist:", error);
