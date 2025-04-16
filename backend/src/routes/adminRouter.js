bf5014372d5687af9e4f5b62738ecc9c62230462
LalitBarage <lalitbarage18092004@gmail.com>
2025-04-17 04:16:19 +0530

    fix: change HTTP method for getSchemeBySubDist route from PUT to GET

--- backend/src/routes/adminRouter.js
+++ backend/src/routes/adminRouter.js
@@ -2,6 +2,6 @@ const express = require("express");
 const { getSchemeBySubDist } = require("../controllers/adminController");
 const router = express.Router();
 
-router.put("/getSchemeBySubDist/:subDist", getSchemeBySubDist);
+router.get("/getSchemeBySubDist/:subDist", getSchemeBySubDist);
 
 module.exports = router;
