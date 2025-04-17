cae028732afbebff386a0bae2a2e643ffbe9d683
LalitBarage <lalitbarage18092004@gmail.com>
2025-04-17 12:50:57 +0530

    feat: add getAlerts functionality to retrieve alerts by location

--- backend/src/routes/alertRouter.js
+++ backend/src/routes/alertRouter.js
@@ -3,11 +3,13 @@ const {
   addCampaign,
   getCampaignsController,
   deleteCampaign,
+  getAlerts,
 } = require("../controllers/alertController");
 const router = express.Router();
 
 router.post("/addCampaign", addCampaign);
 router.get("/getCampaigns", getCampaignsController);
 router.delete("/deleteCampaign/:id", deleteCampaign);
+router.get("/getAlerts/:location", getAlerts);
 
 module.exports = router;
