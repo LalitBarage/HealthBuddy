8eb3c181736be9cae76cb0db2df77b77ad8ab7f8
LalitBarage <lalitbarage18092004@gmail.com>
2025-04-17 13:55:04 +0530

    fix: update parameter name in findDiseaseCountBySubDist function to sub_dist

--- backend/src/models/adminModel.js
+++ backend/src/models/adminModel.js
@@ -65,7 +65,7 @@ const addAlertToDatabase = async (message, severity, location) => {
   }
 };
 
-const findDiseaseCountBySubDist = async (subDist) => {
+const findDiseaseCountBySubDist = async (sub_dist) => {
   try {
     const result = await db.query(
       `SELECT diseases, COUNT(*) as count FROM enquiry WHERE sub_dist = $1 GROUP BY disease`,
