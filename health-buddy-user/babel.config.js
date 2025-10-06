ca07bd030f274a3cda9f573c71c619414c320434
bhuwadatharva <bhuwad.atharva@gmail.com>
2025-10-06 11:32:31 +0530

    user setup done

--- health-buddy-user/babel.config.js
+++ health-buddy-user/babel.config.js
@@ -1,12 +1,19 @@
-module.exports = {
-    presets: ['module:metro-react-native-babel-preset'],
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ["module:metro-react-native-babel-preset"],
     plugins: [
-      ['module:react-native-dotenv', {
-        moduleName: '@env',
-        path: '.env',
-        safe: false,
-        allowUndefined: true,
-      }]
+      [
+        "module:react-native-dotenv",
+        {
+          moduleName: "@env",
+          path: ".env",
+        },
+      ],
+      ["@babel/plugin-transform-class-properties", { loose: true }],
+      ["@babel/plugin-transform-private-methods", { loose: true }],
+      ["@babel/plugin-transform-private-property-in-object", { loose: true }],
+      "react-native-reanimated/plugin", // must stay last
     ],
   };
-  
+};
